fe060462b444e1f3ecb511703db66b7d4401d605
Mosa Cindi <133283513+Kinibuhle@users.noreply.github.com>
2025-09-05 16:27:10 +0200

    changes

--- Backend/init.sql
+++
@@ -1,7 +0,0 @@
-CREAE DATABASE pukkeconnect;
-\c pukkeconnect;// connect to the database
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    fname VARCHAR(100) NOT NULL,
-    lname VARCHAR(100) NOT NULL,
-    
